d6a44d70b3daa2260ef119c74844122bf53a5a60
Mwawaka <kedskeds90@gmail.com>
2022-07-23 10:22:17 +0300

    New:add tailwind css

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+function App() {
+  return (
+    <div className="App">
+      <main>
+      <h1>Redux Toolkit</h1>
+      </main>
+    </div>
+  );
+}
+
+export default App;

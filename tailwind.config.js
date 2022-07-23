92e248a5954d196720102226a347226ec16000e8
Mwawaka <kedskeds90@gmail.com>
2022-07-23 10:39:33 +0300

    Update:'color configuration'

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#645cff",
+        primaryDark: "#282566",
+        primaryLight: "#a29dff",
+        greyOne: "#102a42",
+        greyFive: "#617d98",
+        greyTen: "#f1f5f8",
+        redDark: "hsl(360, 67%, 44%)",
+        redLight: "hsl(360, 71%, 66%)",
+      },
+    },
   },
   plugins: [],
 };

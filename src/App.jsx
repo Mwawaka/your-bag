d0f961631b22b9d57cdb0bc3f7e46632f4add24c
Mwawaka <kedskeds90@gmail.com>
2022-07-28 05:30:02 +0300

    Update:add useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,17 @@
 import React from "react";
 import Navbar from "./components/Navbar";
 import CartContainer from "./components/CartContainer";
+import { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux';
+import { calculateTotals } from './reducers/cartSlice'
+
 
 function App() {
+  const dispatch = useDispatch();
+  const { cartItems } = useSelector(store => store.cart)
+  useEffect(() => {
+    dispatch(calculateTotals());
+  }, [cartItems, dispatch])
   return (
     <main className="text-greyOne">
       <Navbar />

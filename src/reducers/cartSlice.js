bdb6e965ae192fab3f69a8e21f74ad2596511e86
Mwawaka <kedskeds90@gmail.com>
2022-07-28 05:22:42 +0300

    Update:add calculateTotals function

--- src/reducers/cartSlice.js
+++ src/reducers/cartSlice.js
@@ -4,7 +4,7 @@ import cartItems from "../data/cartItems";
 const initialState = {
   cartItems: cartItems,
   amount: 4,
-  total: 30,
+  total: 0,
   isLoading: true,
 };
 
@@ -25,16 +25,29 @@ const cartSlice = createSlice({
       cartItem.quantity += 1;
     },
     decrease: (state, { payload }) => {
-      const cartItem = state.cartItems.find(item => item.id === payload.id);
-      cartItem.quantity -= 1;
+      const cartItem = state.cartItems.find((item) => item.id === payload.id);
+      if (cartItem.quantity > 0) {
+        cartItem.quantity -= 1;
+      } else {
+        cartItem.quantity = 0;
       }
-    //   calculateTotals: () => { }
+    },
+    calculateTotals: (state) => {
+      let amount = 0;
+      let total = 0;
+      state.cartItems.forEach((item) => {
+        amount += item.amount;
+        total += item.amount * item.price;
+      });
+      state.amount = amount;
+      state.total = total;
+    },
 
     // anything returned by the reducer becomes the new state  which replaces the initialState
   },
 });
 // console.log(cartSlice);
 
-export const { clearCart, removeItem, increase,decrease } = cartSlice.actions;
+export const { clearCart, removeItem, increase, decrease,calculateTotals } = cartSlice.actions;
 
 export default cartSlice.reducer;
